33add154f2d482143dfd3d83391371a8ee63b5fc
oyesa <mercyoyesa2018@gmail.com>
2022-05-09 08:51:57 +0300

    update views file to include delete function

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from unicodedata import category
-from flask import Blueprint, flash, render_template, request, flash
+from flask import Blueprint, flash, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
 
 views = Blueprint('views', __name__)
 
@@ -21,3 +22,15 @@ def home():
     db.session.commit()
     flash("Note added!", category='success')
   return render_template('home.html', user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
